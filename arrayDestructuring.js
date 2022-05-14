5e351a83cf3761e7e7a1c656a0b1d74102de89e2
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-14 23:26:51 +0530

    arrayDestructring

---
+++ arrayDestructuring.js
@@ -0,0 +1,41 @@
+
+
+
+// // es5 
+
+//const myproglang = ['js','php','python','java','c'];
+
+// var top1= myproglang[0];
+// var top2 = myproglang[1];
+// var top3 = myproglang[2];
+
+// console.log("my fev prog lang is "+ top1);
+
+
+// es6 
+
+const myproglang = ['js','php','python','java','c'];
+
+
+ //let [top1,top2,top3,top4,top5] = myproglang;
+
+// console.log("my fev prog lang is "+ top1);
+
+console.log(myproglang.length);
+let [top1,,,,top5]=myproglang;
+
+console.log(`my fav prog lang is ${top1} and my last fav is ${top5}`);
+
+
+// swap to number without third variable 
+
+let a =5 ;
+let b = 10;
+
+[a,b] = [b,a];
+
+// let temp = a;
+// a = b;
+// b = temp;
+
+console.log(`the value of a is ${a} and value of b is ${b}`);
