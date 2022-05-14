d3c36a06dcd8149ce7013dc7afab7f2b00f309ae
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-15 00:22:16 +0530

    arrowFunction

--- arrayDestructuring.js
+++ arrayDestructuring.js
@@ -22,7 +22,7 @@ const myproglang = ['js','php','python','java','c'];
 // console.log("my fev prog lang is "+ top1);
 
 console.log(myproglang.length);
-let [top1,,,,top5]=myproglang;
+let [top1,,,,top5]=myproglang;      // yaha pe count kr rhe 1st and last wala element
 
 console.log(`my fav prog lang is ${top1} and my last fav is ${top5}`);
 
---
+++ arrowFunction.js
@@ -0,0 +1,36 @@
+// arrow funcation also called fat function
+
+// es5
+
+var sum = function(){
+    var a =5;
+    var b= 10;
+    return a+b;
+}
+
+console.log(sum());
+
+//  es6
+
+// const sum1 = () =>{
+//     let a = 10;
+//     let b= 20;
+//     return a+b;
+// }
+// console.log(sum1());
+
+//  let a= 10;
+//  let b=20;
+//  const sum2 = () => a+b
+//  console.log(sum2())
+
+
+//  let a = 20;
+//  const sum3 = (b) => a+b
+
+//  console.log(sum3(20));
+
+
+const add = (a,b) => a+b
+
+console.log(add(30,30))
