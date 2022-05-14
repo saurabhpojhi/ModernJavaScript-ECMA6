4bd881ad12733f42276f8530cd37cac466888225
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-15 00:51:46 +0530

    defaultparameter

---
+++ defaultParameter.js
@@ -0,0 +1,30 @@
+// multiply of 2 no
+
+
+//3s6
+// function mult(a,b){
+//     console.log(a*b);
+// }
+
+// mult(4);    // if pass only one value like 4 get output like ...NaN 
+
+// function mult(a,b){
+//    b = (typeof b !== 'undefined') ? b:3
+//     console.log(a*b); 
+// }
+
+// mult(4);    // if pass only one value like 4 get output like 12
+
+// now im Es6
+
+function mult(a,b=2){
+    console.log(a*b);
+}
+mult(4);
+
+
+
+const mult1 = (a,b=4) => {
+    console.log(`${a*b}`);
+}
+mult1(4);
