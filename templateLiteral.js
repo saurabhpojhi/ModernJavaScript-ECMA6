c082bca5f141eb27639288b44e4ade56eb706fcd
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-14 18:01:55 +0530

    Template Literal or Template Stings

---
+++ templateLiteral.js
@@ -0,0 +1,36 @@
+// TemplateLiterals  or Template Strings
+
+// es5
+
+// here we used concatnet to used + sign 
+console.log("my name is saurabh" + " my age is 27");  // output:- my name is saurabh my age is 27
+
+
+// ES6
+
+// here only we used backtick to concatnet the two string 
+console.log(`my name is saurabh. my age is 27.`);   // output:- my name is saurabh my age is 27
+
+// // es5
+
+// let firstName = "saurabh";
+// let lastName = "gupta";
+// console.log("My name is " + firstName + ". My last name is  " + lastName + ".");  // output My name is saurabh. My last name is  gupta.
+
+// es6
+
+let firstName = "saurabh";
+let lastName = "gupta";
+console.log(`My name is ${firstName}. My last name is ${lastName}.`);  // output My name is saurabh. My last name is  gupta.
+
+// // es5
+
+// let a =5 ;
+// let b = 10;
+// console.log('fifteen is '+ (a+b)+ ' and/not' + (2*a+b)+ '.');
+
+// es6 
+let a =5 ;
+let b = 10;
+console.log(`fifteen is ${a+b} and  not  ${2*a+b}.`);
+
