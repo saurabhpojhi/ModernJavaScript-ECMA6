d72acbf1ddc125359fb01822503cba43fef99412
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-15 02:20:37 +0530

    Exponetial Operator

---
+++ ExponentionOperator.js
@@ -0,0 +1,13 @@
+// Exponetial Operator 
+
+// Exponetial Operator (**) raises the first opearnd 
+
+//  x**y produces the same result as math.pow(x,y);
+
+let a =5 ;
+let b = 2;
+console.log(a**b);  // output 25
+
+// or direct we count the power
+
+console.log(10**2);   // output 100
