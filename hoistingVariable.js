b16427b771a68868d34b6ea9575259d3d51aba45
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-22 18:38:32 +0530

    Variable Hoisting

---
+++ hoistingVariable.js
@@ -0,0 +1,23 @@
+// varibale Hoisting 
+
+   // Note : in the function jab v aap call kr skte  but .. varibale me aap agar pahle call kr rhe h baad me calue assign kr rhe tab v uska value aayega undefined.
+    
+
+var a;
+
+console.log(a);   // undefined
+
+var a =10;
+console.log(a);  // 10
+
+console.log(a);
+var a =10;    //   output : undefined
+
+
+var b ;
+b = 10;
+
+console.log(b); // 10
+
+
+
