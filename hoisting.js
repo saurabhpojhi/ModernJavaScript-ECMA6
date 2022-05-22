d62489c0f958e841f51640d5cfc9c698d5e59a8a
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-22 18:19:58 +0530

    Hoisting

--- hoisting.js
+++ hoisting.js
@@ -1,4 +1,33 @@
 
 //   hoisting means Memory to relate 
+// Hoisting is a javascript mechanism where variable and function declaretion are moved to the yop of ther scope before the code execution.
 
+// sum (5,10); 
 
+//  var sum = function  (a,b){
+//     add = a+b;
+//     console.log(add);
+// }
+         // output here got   TypeError: sum is not a function
+        
+         var sum = function  (a,b){
+            add = a+b;
+            console.log(add);
+        }
+        sum (5,10);    // output here got  15 
+
+// sum (5,10); 
+
+// function sum (a,b){
+//     add = a+b;
+//     console.log(add);
+// }                           
+   
+// // or 
+
+// function sum (a,b){
+//     add = a+b;
+//     console.log(add);
+// }
+// sum (5,10);    
+                      //       output here got   15
